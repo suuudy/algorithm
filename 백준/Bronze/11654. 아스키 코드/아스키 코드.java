2ff50a73f0d6b5b0ecfc5f48c865d5b8a176d867
suuudy <44823110+suuudy@users.noreply.github.com>
2023-11-08 13:03:40 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.next();
+        int num = (int)str.charAt(0);
+        System.out.println(num);
+    }
+}
