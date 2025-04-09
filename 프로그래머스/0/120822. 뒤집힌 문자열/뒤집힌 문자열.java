95b1baf11c94097b4a0718a792318844293bfb27
suuudy <44823110+suuudy@users.noreply.github.com>
2025-04-09 16:41:00 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        return sb.reverse().toString();
+    }
+}
